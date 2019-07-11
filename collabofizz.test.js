38133e4406e4842073651ce1c32a57873d1f421e
Carlson, Mike (DS) <mike.carlson@cdk.com>
2019-07-11 13:22:12 -0700

    added files

---
+++ collabofizz.test.js
@@ -0,0 +1,12 @@
+const num = 3;
+
+const fizzbuzz = num => {
+    
+};
+
+/* test */
+describe("fizzbuzz file", () => {
+    it("returns a string", () => {
+        expect(typeof fizzbuzz("")).toBe("string");
+    });
+});
