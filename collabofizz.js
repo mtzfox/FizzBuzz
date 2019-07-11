91c22d045c8c9d8839fea78a6066aa5634a2cd12
Mike C <21209421+redfoxseattle@users.noreply.github.com>
2019-07-11 11:35:32 -0700

    Create collabofizz.js

---
+++ collabofizz.js
@@ -0,0 +1,13 @@
+
+const value = 3;
+
+/* test */
+it("getResult", function(){
+    describes("returns a string", function(){
+			expect("3", )
+    });
+});
+
+const getResult = function(value) {
+    console.log(value.toString);
+}
